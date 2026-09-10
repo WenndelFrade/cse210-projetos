2f87493b036d424522204a85d00ad49e17aedc34
cwenndel <das21013@byui.edu>
2026-09-10 18:40:35 -0300

    sincronizando dados

---
+++ semana02/Curriculo/Emprego.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Emprego
+{
+    static void Main(string[] args)
+    {
+        string _emprego = "Apple";
+        Console.WriteLine(_emprego);
+        string _emprego1 = "Microsoft";
+        Console.WriteLine(_emprego1);
+    }
+}
